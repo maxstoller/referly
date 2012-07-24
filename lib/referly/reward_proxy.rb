5efbdf01694758dc85c9acff76739e392ef10dba
maxstoller <maxstoller@gmail.com>
2012-07-24 15:20:35 -0400

    added defaults for earned_on and payable_on

--- lib/referly/reward_proxy.rb
+++ lib/referly/reward_proxy.rb
@@ -9,6 +9,9 @@ module Referly
     end
 
     def create(params={})
+      params[:earned_on]  ||= Time.now.strftime('%F')
+      params[:payable_on] ||= (Time.now + 2592000).strftime('%F')
+
       @client.post('/rewards', params).parsed_response
     end
   end
